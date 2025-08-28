d7a55f23f5e463e6c741027b5551de181ba8916a
rtboyajian <rtboyajian@gmail.com>
2025-08-28 23:37:23 +0000

    actors commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get("/movies/:path_id", {:controller => "movies", :action => "show"})
   get("/directors", {:controller => "directors", :action => "index"})
   get("/directors/:path_id", {:controller => "directors", :action => "show"})
+  get("/actors", {:controller => "actors", :action => "index"})
+  get("/actors/:path_id", {:controller => "actors", :action => "show"})
 end
