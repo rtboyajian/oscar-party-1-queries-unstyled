4e386a008d1d309970fc706a5cbf79b3d343b31b
rtboyajian <rtboyajian@gmail.com>
2025-08-29 06:47:11 +0000

    oscar 1 final commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   get("/about", { :controller => "misc", :action => "about" })
   get("/movies", {:controller => "movies", :action => "index"})
   get("/movies/:path_id", {:controller => "movies", :action => "show"})
+
   get("/directors", {:controller => "directors", :action => "index"})
+  get("/directors/youngest", {:controller => "directors", :action => "youngest"})
+  get("/directors/eldest", {:controller => "directors", :action => "eldest"})
   get("/directors/:path_id", {:controller => "directors", :action => "show"})
+
   get("/actors", {:controller => "actors", :action => "index"})
   get("/actors/:path_id", {:controller => "actors", :action => "show"})
 end
